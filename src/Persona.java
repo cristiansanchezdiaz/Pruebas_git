a3bf5301b606d8adf6bf7b75300573f7b4e39a1a
cristiansanchezdiaz <cristian.sanchez1@decathlon.com>
2022-05-12 16:42:23 +0200

    Metodo setters and getters

--- src/Persona.java
+++ src/Persona.java
@@ -13,4 +13,36 @@ public class Persona {
         this.dni = dni;
         this.edad = edad;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
 }
