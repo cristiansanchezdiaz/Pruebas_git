67b40aa6c4c7e3d171b7543eff5aebf5f1494c2b
cristiansanchezdiaz <cristian.sanchez1@decathlon.com>
2022-05-12 17:18:57 +0200

    Clase Empleado añadida

---
+++ src/Empleado.java
@@ -0,0 +1,9 @@
+public class Empleado extends Persona {
+
+
+    public Empleado(String nombre, String apellido, String dni, String edad) {
+        super(nombre, apellido, dni, edad);
+    }
+
+    String cargo;
+}
